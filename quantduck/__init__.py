986dc614a1c6fffe108ac6af9516103918e2368a
xin lin <yanrenaicd@gmail.com>
2024-11-16 17:59:59 +0800

    修复问题

--- quantduck/__init__.py
+++ quantduck/__init__.py
@@ -2,4 +2,4 @@
 Quantduck - A quantitative analysis library
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
--- setup.py
+++ setup.py
@@ -1,24 +1,16 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
 setup(
     name="quantduck",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=[
         "psycopg2-binary",
         "pytz",
         "pandas",
     ],
+    description="A quantitative analysis library",
     author="Your Name",
     author_email="your.email@example.com",
-    description="A quantitative analysis library",
-    long_description="Quantduck - A quantitative analysis library",
-    long_description_content_type="text/markdown",
-    url="https://github.com/msjmsj/quantduck",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
 ) 
