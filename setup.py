cf15e237056c173ee8f35d2408d08c7cbf5f1239
xin lin <yanrenaicd@gmail.com>
2024-11-16 17:57:04 +0800

    修复问题

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantduck",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         "psycopg2-binary",
@@ -12,9 +12,9 @@ setup(
     author="Your Name",
     author_email="your.email@example.com",
     description="A quantitative analysis library",
-    long_description=open("README.md").read(),
+    long_description="Quantduck - A quantitative analysis library",
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/quantduck",
+    url="https://github.com/msjmsj/quantduck",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
